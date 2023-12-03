dc7dac5c5a08d3462b91b3fbe1e8a097c85bcb24
Álvaro <alvaromor.alumn@cescristorey.com>
2023-12-03 20:53:08 +0100

    Versión 3.7
    He implementado los test unitarios de eliminar jugador, añadir entrenador y eliminar entrenador.

--- GestionClubsLigaSqliteMaven/src/main/java/Modelo/ConexionMock.java
+++ GestionClubsLigaSqliteMaven/src/main/java/Modelo/ConexionMock.java
@@ -9,10 +9,12 @@ import java.util.ArrayList;
  */
 public class ConexionMock extends Conexion{
     private ArrayList<Jugador> jugadoresBd;
+    private ArrayList<Entrenador> entrenadoresBd;
 
     public ConexionMock(){
         
         jugadoresBd = new ArrayList<>();
+        entrenadoresBd = new ArrayList<>();
     }
 
     public ArrayList<Jugador> getJugadoresBd() {
@@ -22,11 +24,55 @@ public class ConexionMock extends Conexion{
     public void setJugadoresBd(ArrayList<Jugador> jugadoresBd) {
         this.jugadoresBd = jugadoresBd;
     }
+
+    public ArrayList<Entrenador> getEntrenadoresBd() {
+        return entrenadoresBd;
+    }
+
+    public void setEntrenadoresBd(ArrayList<Entrenador> entrenadoresBd) {
+        this.entrenadoresBd = entrenadoresBd;
+    }
     
+    //Simulacion metodos CRUD para la base de datos simulada
+    
+    
+    //Metodos CRUD Jugador
     @Override
     public void insertarJugadorBd(Jugador jugador){
          
         jugadoresBd.add(jugador);
         
     }    
+    
+    /*@Override
+    public void modificarJugadorBd(Jugador jugador){
+        for(int i = 0; i < jugadoresBd.size(); i++){
+            
+        }
+    }*/
+    
+    @Override
+    public void eliminarJugadorBd(String id){
+        for (int i = 0; i < jugadoresBd.size(); i++) {
+            if(jugadoresBd.get(i).getId() == Integer.parseInt(id)){
+                jugadoresBd.remove(i);
+            }
+        }
+    }
+    
+    //Metodos CRUD Entrenador
+    @Override
+     public void insertarEntrenadorBd(Entrenador entrenador){
+        
+        this.entrenadoresBd.add(entrenador);
+    }
+     
+    @Override
+    public void eliminarEntrenadorBd(String id){
+        for (int i = 0; i < entrenadoresBd.size(); i++) {
+            if(entrenadoresBd.get(i).getId().equals(id)){
+                entrenadoresBd.remove(i);
+            }
+        }
+    }
 }
--- GestionClubsLigaSqliteMaven/src/test/java/Controlador/ControladorTest.java
+++ GestionClubsLigaSqliteMaven/src/test/java/Controlador/ControladorTest.java
@@ -73,4 +73,134 @@ public class ControladorTest {
         
     }
     
+    /**
+     * Test of modificarJugador method, of class Controlador.
+     */
+    /*@Test
+    public void testModificarJugador() throws Exception {
+        System.out.println("modificarJugador");
+        
+        ConexionMock conexionMock = new ConexionMock();
+        Controlador controlador = new Controlador();
+        controlador.configurarConexion(conexionMock);
+        
+                // Crear un jugador para la prueba
+        Jugador jugadorExistente = new Jugador();
+        jugadorExistente.setId(1); // Supongamos que el jugador con ID 1 ya existe
+        jugadorExistente.setNombre("Lionel");
+        jugadorExistente.setApellido("Messi");
+        jugadorExistente.setanio_nacimiento("1987");
+        jugadorExistente.setNacionalidad("Argentina");
+        jugadorExistente.setPosicion("Delantero");
+        jugadorExistente.setSalario(1000000.0);
+        jugadorExistente.setId_club(1);
+
+        // Añadir el jugador existente a la lógica simulada de la conexión
+        conexionMock.getJugadoresBd().add(jugadorExistente);
+
+        // Crear un nuevo objeto con los datos modificados
+        Jugador jugadorModificado = new Jugador();
+        jugadorModificado.setId(1); // Mismo ID que el jugador existente
+        jugadorModificado.setNombre("Lionel");
+        jugadorModificado.setApellido("Messi");
+        jugadorModificado.setanio_nacimiento("1987");
+        jugadorModificado.setNacionalidad("Argentina");
+        jugadorModificado.setPosicion("Delantero");
+        jugadorModificado.setSalario(1500000.0); // Salario modificado
+        jugadorModificado.setId_club(1);
+
+        // Ejecución de la prueba
+        controlador.modificarJugador(jugadorModificado.getId(), jugadorModificado.getNombre(), jugadorModificado.getApellido() , jugadorModificado.getanio_nacimiento(), jugadorModificado.get);
+
+        // Verificación de los resultados
+        assertEquals(1, conexionMock.getJugadoresSimulados().size());
+        assertEquals(jugadorModificado, conexionMock.getJugadoresSimulados().get(0));
+    }*/
+    
+
+    
+    
+    /**
+     * Test of removeJugador method, of class Controlador.
+     */
+    @Test
+    public void testRemoveJugador() throws Exception {
+        System.out.println("removeJugador");
+
+        ConexionMock conexionMock = new ConexionMock();
+        Controlador controlador = new Controlador();
+        controlador.configurarConexion(conexionMock);
+        
+        Jugador jugador = new Jugador();
+        jugador.setId(1); // Supongamos que el jugador con ID 1 ya existe
+        jugador.setNombre("Lionel");
+        jugador.setApellido("Messi");
+        jugador.setanio_nacimiento("1987");
+        jugador.setNacionalidad("Argentina");
+        jugador.setPosicion("Delantero");
+        jugador.setSalario(1000000.0);
+        jugador.setId_club(1);
+        
+        conexionMock.getJugadoresBd().add(jugador);
+        
+        controlador.removeJugador(Integer.toString(jugador.getId()));
+        conexionMock.close();
+        
+        assertEquals(0, conexionMock.getJugadoresBd().size());
+        
+    }
+    
+        /**
+     * Test of addEntrenador method, of class Controlador.
+     */
+    @Test
+    public void testAddEntrenador() throws Exception {
+        System.out.println("addEntrenador");
+        ConexionMock conexionMock = new ConexionMock();
+        Controlador controlador = new Controlador();
+        controlador.configurarConexion(conexionMock);
+
+        // Crear un entrenador para la prueba
+        Entrenador entrenador = new Entrenador();
+        entrenador.setId("0");
+        entrenador.setNombre("Pep");
+        entrenador.setApellido("Guardiola");
+        entrenador.setAnio_nacimiento("1971");
+        entrenador.setNacionalidad("España");
+
+        // Ejecución de la prueba
+        controlador.addEntrenador(entrenador.getNombre(), entrenador.getApellido(), entrenador.getAnio_nacimiento(), entrenador.getNacionalidad());
+        conexionMock.close();
+
+        // Verificación de los resultados
+        assertEquals(1, conexionMock.getEntrenadoresBd().size());
+    }
+    
+    /**
+     * Test of removeEntrenador method, of class Controlador.
+     */
+    @Test
+    public void testRemoveEntrenador() throws Exception {
+        System.out.println("removeEntrenador");
+        ConexionMock conexionMock = new ConexionMock();
+        Controlador controlador = new Controlador();
+        controlador.configurarConexion(conexionMock);
+
+        // Crear un entrenador para la prueba
+        Entrenador entrenador = new Entrenador();
+        entrenador.setId("0");
+        entrenador.setNombre("Pep");
+        entrenador.setApellido("Guardiola");
+        entrenador.setAnio_nacimiento("1971");
+        entrenador.setNacionalidad("España");
+
+        conexionMock.getEntrenadoresBd().add(entrenador);
+        
+        // Ejecución de la prueba
+        controlador.removeEntrenador(entrenador.getId());
+        conexionMock.close();
+
+        // Verificación de los resultados
+        assertEquals(0, conexionMock.getEntrenadoresBd().size());
+    }
 }
